43149c78bc72153bd33e864873e72a06e6a7a055
Staci Holmes-Segarra <stacihs@hotmail.com>
2019-01-30 08:45:03 -0600

    Set initial map view

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import MapView from './components/MapView';
+import locations from './data/locations';
 import './App.css';
 
 class App extends Component {
+  state = {
+    lat: 29.4428476,
+    lon: -98.4800684,
+    zoom: 14,
+    locations: locations
+  }
   render() {
     return (
       <div className="App">
+        <div>
+          <h1>The Pearl Neighborhood</h1>
+          <h2>San Antonio, TX</h2>
+        </div>
+        <MapView
+          lat={this.state.lat}
+          lon={this.state.lon}
+          zoom={this.state.zoom}
+        />
       </div>
+      
     );
   }
 }
 
 export default App;
+  
