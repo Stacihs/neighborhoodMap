5544aca654e019b82cb7359bdb931dbd5b8c3355
Staci Holmes-Segarra <stacihs@hotmail.com>
2019-02-06 08:54:07 -0600

    Add map and list components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
 import MapView from './components/MapView';
-import locations from './data/locations';
-import './App.css';
+// import ListView from '.components/ListView';  
+import './App.css'
 
 class App extends Component {
-  state = {
-    lat: 29.4428476,
-    lon: -98.4800684,
-    zoom: 14,
-    locations: locations
-  }
+  
   render() {
     return (
       <div className="App">
@@ -18,10 +13,9 @@ class App extends Component {
           <h2>San Antonio, TX</h2>
         </div>
         <MapView
-          lat={this.state.lat}
-          lon={this.state.lon}
-          zoom={this.state.zoom}
         />
+        {/* <ListView
+        /> */}
       </div>
       
     );
