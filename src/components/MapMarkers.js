1e1c2014398b1c90e79ed9dfcf4d2917c584f993
Staci Holmes-Segarra <stacihs@hotmail.com>
2019-02-08 11:49:32 -0600

    Add component

---
+++ src/components/MapMarkers.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MapMarkers extends Component {
+
+    // retrieve locations[]
+    // create markers from locations[]
+    // change marker style when selected
+    // add info window to markers
+    // additional custom functionality
+
+
+    render() {
+        return 
+    }
+}
+export default MapMarkers;
+  
