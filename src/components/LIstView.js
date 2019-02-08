992d33cd21c6ba8e31b87bc6a2de1f8a1542ad43
Staci Holmes-Segarra <stacihs@hotmail.com>
2019-02-08 11:47:46 -0600

    Add component

---
+++ src/components/LIstView.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ListView extends Component {
+
+   
+    text input field
+     -filters map markers and list items(error-free)
+    create list
+    clicking location shows infoWindow and animates its map markers
+    make responsive
+
+    
+
+
+    render() {
+        return 
+    }
+}
+export default ListView;
