064b87d24030bc87c5173b91a851f0dd79d6bf74
Staci Holmes-Segarra <stacihs@hotmail.com>
2019-02-08 11:46:57 -0600

    Change component name to better reflect its use

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
-import MapView from './components/MapView';
-// import ListView from '.components/ListView';  
+import MapContainer from './components/MapContainer';
+// import locations from './data/locations.json'
+// import ListView from './components/ListView';
 import './App.css'
 
 class App extends Component {
   state = {
-    lat: 29.4428476,
-    lng: -98.4800684
   }
   
   render() {
     return (
       <div className="App">
         <div>
-          <h1>The Pearl Neighborhood</h1>
-          <h2>San Antonio, TX</h2>
+          <h1>The Pearl Neighborhood - San Antonio,TX</h1>
         </div>
-        <MapView
+        <MapContainer
         />
         {/* <ListView
         /> */}
